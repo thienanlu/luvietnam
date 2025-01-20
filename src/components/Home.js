4818b6422a2ab313f5614e8d18de2aa6f431af80
thienanlu <timothy.thienanjw@gmail.com>
2025-01-20 19:37:31 +0700

    test commit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+// import components
+import Header from "./Header";
+import Footer from "./Footer";
+
+
+function Home(){
+    return(
+        <div className="home-wrapper">
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home;
